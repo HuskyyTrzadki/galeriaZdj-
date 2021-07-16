73f8f2e351556da34cddcfa0f8c7bef367dddabc
HuskyyTrzaski <kubajochymek@gmail.com>
2021-07-16 21:35:00 +0200

    dodanie inputa

---
+++ pseudo-searcher/src/components/App.js
@@ -0,0 +1,8 @@
+import SearchBar from "./SearchBar"; 
+const App=()=>{
+    
+    return (<div>
+        <SearchBar/ >
+    </div> )    
+};
+export default App;
---
+++ pseudo-searcher/src/components/SearchBar.js
@@ -0,0 +1,13 @@
+//text input na gorze
+ //musze uzyc steata
+ import React from 'react';
+ class SearchBar extends React.Component{
+     render(){
+         return (<div>
+             <form>
+                 <input type="text"/> 
+             </form>
+             </div>);
+     }
+ }
+ export default SearchBar; 
