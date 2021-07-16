25e34544230afffffac27305e901a5eb9068b647
HuskyyTrzaski <kubajochymek@gmail.com>
2021-07-16 23:27:14 +0200

    control instead of incontrol

--- pseudo-searcher/src/components/SearchBar.js
+++ pseudo-searcher/src/components/SearchBar.js
@@ -2,16 +2,23 @@
  //musze uzyc steata
  import React from 'react';
  class SearchBar extends React.Component{
+     state={term:''};
      
      render(){
+         //nie chcemy trzymac info w DOM, stad ten refactroing
+         //jakbym chcial dowiedziec sie jaka wartosc jest teraz, to nie musze siegac do DOM
+         //chcemy po prostu wiecej reacta mniej dom
+         //kontrolowany~!
+         //controlled flow
          return (<div className="ui segment">
              <form className="ui form">
                  <div className="field">
                      <label>Image Search</label>
-                     <input type="text" onChange={(e)=>{console.log(e.target.value)}}/> 
-                 </div> 
-             </form>
+                     <input type="text" value ={this.state.term} onChange={(e)=>this.setState
+                        ({term:e.target.value.toUpperCase()})}/> 
+                 </div>  
+             </form>  
              </div>);
      }
- }
+ } 
  export default SearchBar; 
