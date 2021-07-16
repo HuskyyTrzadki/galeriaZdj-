d9fb8860fad2d7ff3c6e841bf23899c1b18fb00f
HuskyyTrzaski <kubajochymek@gmail.com>
2021-07-17 00:09:36 +0200

    blad z this:

--- pseudo-searcher/src/components/SearchBar.js
+++ pseudo-searcher/src/components/SearchBar.js
@@ -3,7 +3,12 @@
  import React from 'react';
  class SearchBar extends React.Component{
      state={term:''};
-     
+     onFormSubmit(event){
+         event.preventDefault(); // nigdy wlaciwie nie chcemy z eby sie odswiezalo(czeste)
+         console.log(this.state.term);//problem z this
+         //!!!!!!!!!!! PROBLEM Z THIS !!! czesty 
+         
+     }
      render(){
          //nie chcemy trzymac info w DOM, stad ten refactroing
          //jakbym chcial dowiedziec sie jaka wartosc jest teraz, to nie musze siegac do DOM
@@ -11,7 +16,7 @@
          //kontrolowany~!
          //controlled flow
          return (<div className="ui segment">
-             <form className="ui form">
+             <form onSubmit={this.onFormSubmit} className="ui form">
                  <div className="field">
                      <label>Image Search</label>
                      <input type="text" value ={this.state.term} onChange={(e)=>this.setState
